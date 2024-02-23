750ae282d3b65cd39245023208ba341ec4dfd2e6
miok <aluvy_@naver.com>
2024-02-23 11:24:22 +0900

    whoweare

--- front/src/views/MainPage.vue
+++ front/src/views/MainPage.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div id="main-page">
     <swiper
       class="mySwiper"
       :class="{moveStart: moveStart, moveEnd: !moveStart, up: !direction, down: direction}"
@@ -51,6 +51,8 @@
 </template>
 
 <script>
+import '@/assets/css/page-main.css'
+
 import { ref } from 'vue';
 import { Swiper, SwiperSlide } from 'swiper/vue';
 import { Autoplay, Pagination, A11y, Virtual } from 'swiper/modules';
@@ -144,117 +146,3 @@ export default {
   },
 }
 </script>
-
-<style>
-:root {
-  --mainSwiper-easing: cubic-bezier(0.4, 0, 0.2, 1);
-  --mainSwiper-delay: 500ms;
-
-  /* img scale */
-  --mainSwiper-img-scale-duration: 600ms;
-
-  /* dimmed */
-  --mainSwiper-dimmed-duration: 400ms;
-
-    /* slogan */
-  --mainSwiper-active-slogan-duration: 600ms;
-  --mainSwiper-active-slogan-delay: 0ms;
-  --mainSwiper-prev-slogan-duration: 600ms;
-  --mainSwiper-prev-slogan-delay: 400ms;
-}
-
-.mySwiper { width: 100%; height: var(--vh); background: #000; overflow: hidden; z-index: 4; }
-.mySwiper .swiper-wrapper { transition-delay: var(--mainSwiper-delay); transition-timing-function: var(--mainSwiper-easing); }
-.mySwiper .swiper-slide { position: relative; width: 100%; height: var(--vh); transition-timing-function: ease-out; }
-
-/*** imgarea ******************************************************/
-.mySwiper .swiper-slide .img-area { width: 100%; height: var(--vh); z-index: 1; }
-.mySwiper .swiper-slide .img-area .img { position: relative; width: 100%; height: 100%; background-position: center; background-size: cover; background-repeat: no-repeat; transform: scale(0.6); transition-property: transform; transition-duration: var(--mainSwiper-img-scale-duration); transition-timing-function: var(--mainSwiper-easing); }
-
-/* imgarea - img - scale */
-.mySwiper.moveStart .swiper-slide .img-area .img { transform: scale(0.5); }
-.mySwiper.moveEnd .swiper-slide.swiper-slide-active .img-area .img { transform: scale(1.01); }
-
-/*** imgarea - img - dimmed ******************************************************/
-.mySwiper .swiper-slide .img-area .img::before { position: absolute; left: 0; top: 0; display: block; content:''; width: 100%; height: 100%; background: rgba(0,0,0, 0.4); opacity: 1; transition-property: opacity; transition-duration: var(--mainSwiper-dimmed-duration); }
-.mySwiper.moveStart .swiper-slide .img-area .img::before { opacity: 0; }
-
-/*** slogan-area ******************************************************/
-.mySwiper .slogan-area { position: absolute; left: 50%; top: 50%; transform: translate3D(-50%, -50%, 0); width: 100%; max-width: var(--max-width); padding: var(--inner-pad); max-height: var(--vh); z-index: 2; }
-.mySwiper .slogan-area .slogan { display: flex; flex-wrap: wrap; align-items: center; width: 50%; font-size: 6.3rem; line-height: 1.2; font-weight: 700; color: #fff; }
-.mySwiper .slogan-area .slogan strong { display: inline-block; overflow: hidden; padding-right: 2rem; }
-.mySwiper .slogan-area .slogan strong span { display: inline-block; width: fit-content; word-break: keep-all; transition-property: transform; }
-
-/* slogan - active motion */
-.mySwiper .swiper-slide-active .slogan-area .slogan strong span { transform: translate3D(0, 0, 0); transition-duration: var(--mainSwiper-active-slogan-duration); transition-delay: var(--mainSwiper-active-slogan-delay); }
-.mySwiper.moveStart.down .swiper-slide-active .slogan-area .slogan strong span { transform: translate3D(0, 120%, 0); }
-.mySwiper.moveStart.up .swiper-slide-active .slogan-area .slogan strong span { transform: translate3D(0, -120%, 0);}
-
-/* slogan - prev & next motion */
-.mySwiper .swiper-slide-prev .slogan-area .slogan strong span,
-.mySwiper .swiper-slide-next .slogan-area .slogan strong span { transition-duration: var(--mainSwiper-prev-slogan-duration); transition-delay: var(--mainSwiper-prev-slogan-delay); }
-.mySwiper.moveStart.down .swiper-slide-prev .slogan-area .slogan strong span { transform: translate3D(0, -120%, 0); }
-.mySwiper.moveStart.up .swiper-slide-next .slogan-area .slogan strong span { transform: translate3D(0, 120%, 0); }
-
-/* slogan - link */
-.mySwiper .slogan-area .linkto { font-size: 1.6rem; color: #fff; line-height: 1.9; letter-spacing: 0.065em; margin: 2.5rem 0 0; overflow: hidden; }
-.mySwiper .slogan-area .linkto a { position: relative; display: block; width: fit-content; opacity: 0; transition-property: transform, opacity; transition-duration: 0.5s, 0.3s; transition-delay: var(--mainSwiper-prev-slogan-delay); }
-
-/* slogan - link - active motion */
-.mySwiper.moveEnd .slogan-area .linkto a { transform: translate3D(0, 0, 0); opacity: 1; }
-.mySwiper.moveStart.down .slogan-area .linkto a { transform: translate3D(0, 110%, 0); }
-.mySwiper.moveStart.up .slogan-area .linkto a { transform: translate3D(0, -110%, 0); }
-
-/* slogan - link - prev & next motion */
-.mySwiper.moveStart .swiper-slide-prev .slogan-area .linkto a,
-.mySwiper.moveStart .swiper-slide-next .slogan-area .linkto a { transition-delay: var(--mainSwiper-active-slogan-delay); }
-.mySwiper.moveStart.down .swiper-slide-prev .slogan-area .linkto a { transform: translate3D(0, -110%, 0); }
-.mySwiper.moveStart.up .swiper-slide-next .slogan-area .linkto a { transform: translate3D(0, 110%, 0); }
-
-/* slogan - link hover motion */
-.mySwiper .slogan-area .linkto a::after { position: absolute; left: 0; bottom: 0; content: ''; display: block; width: 0; height: 2px; background: #ffffff; transition: width .3s; }
-.mySwiper .slogan-area .linkto a:hover::after { width: 100%; }
-
-
-/*** pagination ******************************************************/
-.swiper-pagination { position: fixed; right: 50%; top: 50%; margin-right: 9rem; transform: translate3D( calc(var(--max-width) / 2) , -50%, 0); display: flex; flex-direction: column; align-items: flex-end; z-index: 9; }
-.swiper-pagination-bullet { padding: 0.8rem 0; }
-.swiper-pagination-bullet::before { content:''; display: block; width: 1.5rem; height: 2px; background: #fff; opacity: 0.5; transition: all .3s; }
-.swiper-pagination-bullet-active::before { width: 3rem; opacity: 1; }
-
-
-/*** controller ******************************************************/
-.slide-controller { position: fixed; left: 50%; bottom: 5rem; margin-left: 9rem; transform: translateX(calc(var(--max-width) / 2 * -1)); display: flex; align-items: center; z-index: 5; }
-.slide-controller button { position: relative; width: 4.4rem; height: 4.4rem; font-size: 1rem; line-height: 1.2rem; color: #fff; border-radius: 100%; border: 1px solid rgba(255,255,255, 0.5); }
-.slide-controller button + button { margin-left: 1.4rem; }
-.slide-controller button i { transition: all .3s var(--ease-InOutExpo); }
-.slide-controller button::after { position: absolute; left: calc(50% - 0.7rem); top: calc(50% - 0.8px); content:''; display: block; width: 1.4rem; height: 1px; background: #fff; transform: scaleX(0); transition: transform 0.3s var(--ease-InOutExpo); }
-.slide-controller button:hover::after { transform: scaleX(1); }
-.slide-controller button.btn_prev:hover i { transform: translate3D(-0.6rem, 0, 0); }
-.slide-controller button.btn_next:hover i { transform: translate3D(0.6rem, 0, 0); }
-
-
-@media only screen and (max-width: 1600px) {
-  .swiper-pagination { right: 9rem; margin-right: 0; transform: translate3D(0, -50%, 0); }
-  .slide-controller { left: 9rem; margin-left: 0; transform: initial; }
-}
-
-@media only screen and (max-width: 1024px) {
-  .mySwiper .slogan-area .slogan { font-size: 5.88rem; }
-  .swiper-pagination { right: 50%; margin-right: 4rem; transform: translate3D( calc(var(--max-width) / 2) , -50%, 0); }
-  .slide-controller { left: 50%; margin-left: 4rem; transform: translateX(calc(var(--max-width) / 2 * -1)); }
-}
-
-@media only screen and (max-width: 768px) {
-  .mySwiper .slogan-area .slogan { width: 100%; font-size: 4.8rem; }
-  .swiper-pagination { display: none; }
-  .slide-controller { left: 4rem; margin-left: 0; transform: initial; }
-}
-
-/* media query - height */
-@media only screen and (max-height: 650px) {
-  .mySwiper .slogan-area .slogan { width: calc(100% - 6rem); font-size: 4.4rem; }
-  .mySwiper .slogan-area .linkto { margin-top: 1rem; }
-  .slide-controller { bottom: 3rem; }
-}
-</style>
--- front/src/views/WhoWeArePage.vue
+++ front/src/views/WhoWeArePage.vue
@@ -1,14 +1,104 @@
 <template>
-  <div>
-    who we are
+  <div id="who-page" ref="elem">
+
+    <section class="section black who-visual">
+      <div class="slogan">
+        <div class="slogan-inner">
+          <ContentTitle
+            titleS="WHO WE ARE"
+            :titleL="['Special Group For', 'Digital Marketing',]"
+            desc="더피프티원은 매년 다양한 프로젝트 경험과 전문성 있는 인적자원으로 프로젝트를 성공적으로 진행하며 업계를 선도합니다."
+            theme="black"
+          ></ContentTitle>
+        </div>
+      </div>
+      <div class="bg gsap-item"></div>
+    </section>
+
+    <section class="section who-overview">
+      <div class="inner">
+        <ContentTitle
+          titleS="Overview"
+          :titleL="[`There Are Many Numbers In Between ‘0’ And ‘100’`,]"
+          theme="black"
+        ></ContentTitle>
+        <ul class="desc">
+          <li>
+            '0'과 '100'사이에는 많은 숫자가 존재합니다.<br>
+            '100'이 모든것을 이루어낸 '완성'을 의미한다면 '0'은 이루어나가야 하는 '미완성'을 의미합니다.<br>
+            누군가는 모든것을 이루어낸 '100'에 큰 의미를 부여하지만 우리는 '0'과 '100'사이의<br>
+            '51(fifty-one)'에 주목합니다. '51'은 가능과 불가능을 구분짓는 '가능성'을 의미하기 때문입니다. 불가능을 가능으로 만드는 '51'의 순간을 우리는 함께 경험할 것입니다.
+          </li>
+          <li>
+            If '100' represents 'complete' that achieved everything '0' represents 'incomplete' that has to achieve everything While someone gives significant meaning to '100' we focus on '51(fifty-one)' between '0' and '100'<br>
+            '51' represents 'possibility' that defferentiates possible and impossible We will experience the moment of '51' making the impossible to possible all together.
+          </li>
+        </ul>
+        <ul class="info">
+          <li class="gsap-item">
+            <strong>Company</strong>
+            <p>(주)더피프티원</p>
+            <p>조종영 대표이사</p>
+            <p>2011년 4월 11일(법인설립)</p>
+            <p>80명(자회사 정규직원 포함)</p>
+          </li>
+          <li class="gsap-item">
+            <strong>Business Field</strong>
+            <p>디지털마케팅 에이전시</p>
+            <p>디지털 콘텐츠 서비스</p>
+            <p>UX컨설팅 서비스</p>
+          </li>
+          <li class="gsap-item">
+            <strong>Contact</strong>
+            <p>Head Office</p>
+            <p>서울특별시 강남구</p>
+            <p>봉은사로18길 58</p>
+          </li>
+        </ul>
+      </div>
+    </section>
+
+    <section class="section white who-service">
+      <div class="inner">
+        swiper
+      </div>
+      <div class="bg"></div>
+    </section>
+
+    <section class="section white who-client">
+      <div class="inner">
+        <ContentTitle
+          titleS="Our Clients"
+          :titleL="['These Are The Customers', 'Who Trusted Us.',]"
+          theme="white"
+          align="center"
+        ></ContentTitle>
+      </div>
+    </section>
+
   </div>
 </template>
 
 <script>
-export default {
+import '@/assets/css/page-who.css'
 
+import { gsapScrollTrigger, gsapKill } from '@/utils/gsap.js'
+
+import ContentTitle from '@/components/common/ContentTitle.vue'
+
+export default {
+  components: {
+    ContentTitle,
+  },
+  async mounted() {
+    await this.$nextTick();
+    setTimeout(() => {
+      const elem = this.$refs.elem;
+      gsapScrollTrigger(elem);
+    }, 1);
+  },
+  unmounted() {
+    gsapKill();
+  },
 }
 </script>
-
-<style>
-</style>
