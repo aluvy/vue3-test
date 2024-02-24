3cd306a7013072b200e9f3a3f9b825adc6b36e94
miok <aluvy_@naver.com>
2024-02-24 20:42:29 +0900

    수정

--- front/src/views/WorkPage.vue
+++ front/src/views/WorkPage.vue
@@ -3,25 +3,15 @@
     
     <section class="section black work-visual" data-theme="light">
       <ContentVisual>
-        <template v-slot:title>
+        <template #title>
           <ContentTitle
-            :titleXL="[
-              { delay: 2, text: `Finding the right one is` },
-              { delay: 3, text: `what we do best.` }
-            ]"
+            :titleXL="[ { delay: 2, text: `Finding the right one is` }, { delay: 3, text: `what we do best.` } ]"
             :descL="[ { delay: 4, text: `What We Made` } ]"
             theme="black"
             align="center"
           ></ContentTitle>
-          <div class="scroll-down-wrap no-border">
-            <a href="#" class="section-down-arrow ">
-              <svg class="nectar-scroll-icon" viewBox="0 0 30 45" enable-background="new 0 0 30 45">
-                <path class="nectar-scroll-icon-path" fill="none" stroke="#ffffff" stroke-width="2" stroke-miterlimit="10" d="M15,1.118c12.352,0,13.967,12.88,13.967,12.88v18.76  c0,0-1.514,11.204-13.967,11.204S0.931,32.966,0.931,32.966V14.05C0.931,14.05,2.648,1.118,15,1.118z"></path>
-              </svg>
-            </a>
-          </div>
         </template>
-        <template v-slot:visual>
+        <template #visual>
           <video preload="auto" loop autoplay muted playsinline><source :src="video" type="video/mp4"></video>
         </template>
       </ContentVisual>
