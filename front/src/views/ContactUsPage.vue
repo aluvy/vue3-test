a9bfcd11f7959749c90db6f86516b68a1761202b
miok <aluvy_@naver.com>
2024-03-04 17:27:56 +0900

    contactus

--- front/src/views/ContactUsPage.vue
+++ front/src/views/ContactUsPage.vue
@@ -21,8 +21,31 @@
     </section>
 
     <div id="content">
+
+      <section class="section contact" data-theme="light">
+        <div id="map" ref="map">맵</div>
+        <div class="location">
+          <ContentTitle
+            :titleL="[
+              { delay: 1, text: `Location.` },
+            ]"
+            :desc = "[
+              { delay: 2, text: `우리에게 의뢰할 프로젝트가 있으신가요? 혹은 우리와 함께 성장하고 싶으신가요?`},
+              { delay: 3, text: `방문, 전화, 팩스, 메일로 얼마든 문의주세요` }
+            ]"
+            theme="black"
+          ></ContentTitle>
+          <div class="cont">
+            <strong>(주)더피프티원</strong>
+            <p>서울특별시 강남구 봉은사로18길 58</p>
+            <p>Tel: 02 332 6251 / 2</p>
+            <p>Fax: 02 332 6253</p>
+            <p>Email: contact@the-51.com</p>
+          </div>
+        </div>
+      </section>
       
-      <section class="section who-link" data-theme="light">
+      <section class="section contact-link" data-theme="light">
         <ul>
           <li>
             <router-link to="/who-we-are">CONTACT US</router-link>
