154d38a8728e8f15751ddf3ce02a1b6ea9e8b70f
miok <aluvy_@naver.com>
2024-03-07 12:13:56 +0900

    carousel benefit slide

--- front/src/components/common/SwiperBenefit_old.vue
+++ front/src/components/common/SwiperBenefit_old.vue
@@ -1,6 +1,7 @@
 <template>
   <swiper
     class="bfSwiper"
+    ref="bfSwiper"
     :modules="modules"
     :spaceBetween="0"
     :slidesPerView="'auto'"
@@ -49,10 +50,11 @@ export default {
   },
   setup() {
     let mySwiper = ref();
+    // let bfSwiper = ref();
 
     const onSwiper = (swiper) => {
       mySwiper.value = swiper;
-      console.log(swiper.activeIndex);
+      // console.log(swiper.activeIndex);
 
       
       // swiper.loopDestroy();
@@ -75,9 +77,17 @@ export default {
       // console.log('observerUpdate');
     }
 
-    const beforeResize = () => {
-      // console.log('beforeResize', swiper);
+    const beforeResize = (swiper) => {
+      console.log('beforeResize', swiper);
+      // swiper.attachEvents();
+      // swiper.destroy(false, true);
+      // onSwiper();
+      // swiper.init(bfSwiper.value);
+      swiper.update();
+      // swiper.updateSize();
+      // swiper.updateSlides();
       // swiper.slideTo(swiper.activeIndex);
+      // console.log(onSwiper);
     }
 
     return {
@@ -91,6 +101,38 @@ export default {
       beforeResize
     }
   },
+  // methods: {
+  //   swiperInit() {
+  //     const bfSwiper = this.$refs.bfSwiper;
+  //     const options = {
+  //       // :modules="modules"
+  //       spaceBetween: 0,
+  //       slidesPerView: 'auto',
+  //       activeIndex: 0,
+  //       centeredSlides: true,
+  //       loopL: true,
+
+  //       draggable: true,
+  //       speed: 300,
+  //       pagination: {
+  //         clickable: true,
+  //         bulletElement: 'button'
+  //       },
+        
+  //       // @swiper="onSwiper"
+  //       // @transitionStart="transitionStart"
+  //       // @transitionEnd="transitionEnd"
+  //       // @observerUpdate="observerUpdate"
+  //       // @beforeResize="beforeResize"
+  //     }
+
+  //     new Swiper(bfSwiper, options);
+  //     console.log(bfSwiper);
+  //   },
+  // },
+  // mounted() {
+  //   console.log('mounted', this.swiperInit);
+  // }
 }
 </script>
 
@@ -98,7 +140,7 @@ export default {
 /* service | swiper */
 .bfSwiper { position: relative; width: 100%; overflow: hidden; touch-action: pan-y; margin-top: 8rem; }
 .bfSwiper .swiper-wrapper { position: relative; width: 100%; height: 100%; display: flex; z-index: 1; transition-property: transform; }
-.bfSwiper .swiper-slide { position: relative; width: 75%; height: 100%; flex-shrink: 0; opacity: 0.3; transition: opacity .3s; }
+.bfSwiper .swiper-slide { position: relative; width: 70%; height: 100%; flex-shrink: 0; opacity: 0.3; transition: opacity .3s; }
 .bfSwiper .bubble { position: relative; color: #000; font-size: 1.4rem; line-height: 1.6; padding: 5rem; padding-left: 10rem; border-radius: 1rem; }
 .bfSwiper .bubble::before { position: absolute; left: 3rem; top: 0; content: open-quote open-quote; letter-spacing: -0.8rem; font-size: 13rem; color: rgba(0,0,0, 0.4); font-family: 'sans-serif'; }
 .bfSwiper .bubble::after { position: absolute; left: calc(50% - 1.5rem); bottom: -1rem; content: ''; display: block; width: 3rem; height: 3rem; border-radius: 0.6rem; background: #6d6d6d; transform: rotate(45deg); opacity: 0; }
@@ -129,5 +171,8 @@ export default {
 }
 @media only screen and (max-width: 768px) {
   /* .bfSwiper { padding: 0; } */
+}
+@media only screen and (max-width: 425px) {
+  .bfSwiper .swiper-slide { width: 100%; }
 }
 </style>
--- front/src/views/CulturePage.vue
+++ front/src/views/CulturePage.vue
@@ -61,7 +61,7 @@
           :titleL = "[ { delay: 0, text: `Benefits` } ]"
           align="center"
         ></ContentTitle>
-        <SwiperBenefit :slides="benefits"></SwiperBenefit>
+        <CarouselBenefit :slides="benefits"></CarouselBenefit>
       </section>
 
     </div>
@@ -75,7 +75,7 @@ import PageMixin from '@/mixins/PageMixin';
 // component
 import ContentVisual from '@/components/common/ContentVisual.vue'
 import ContentTitle from '@/components/common/ContentTitle.vue'
-import SwiperBenefit from '@/components/common/SwiperBenefit.vue'
+import CarouselBenefit from '@/components/common/CarouselBenefit.vue'
 
 // assets
 import visual from '@/assets/images/culture/visual.jpg'
@@ -92,7 +92,7 @@ export default {
   components: {
     ContentVisual,
     ContentTitle,
-    SwiperBenefit,
+    CarouselBenefit,
   },
   data() {
     return {
@@ -111,7 +111,6 @@ export default {
         { title: 'Family', desc: '언제나 최고의 결과를 만들어내기 위해 노력하는 직원들의 뒤에는 가족이라는 든든한 지원군이 있습니다. 더피프티원은 가족과 함께하는 시간에 즐거움을 더하기 위해 구정과 추석에 상품권을 증정합니다.', },
         { title: 'Workshop & Party', desc: '매년 봄과 가을에 워크숍을 진행해, 평소 프로젝트 파견으로 모이기 어려운 직원들이 한자리에 모여 서로에 대해 알아가고 팀워크를 강화하는 시간을 가집니다. 12월에는 한 해를 마무리하는 송년 파티를 열어 일 년 동안 수고한 직원들에게 감사를 표하고, 인센티브와 상장 및 상금 수여식을 진행합니다.', },
         { title: 'Study', desc: '기획, 디자인, 퍼블리셔, 개발 직군 별로 체계적인 사내 교육을 진행합니다. 누구나 교육에 참여해 업무에 필요한 지식을 쌓고, 평소 파견 업무로 만나기 어려운 팀원들을 만나 더욱 가까워질 수 있습니다. 사내교육에 필요한 교재와 간식비, 저녁식사 비용이 기본으로 지원되며, 교육을 이수한 직원과 사내 강사에게는 인센티브 및 강사비가 지급됩니다.', },
-        { title: 'Vacation', desc: '무더운 여름, 더피프티원 직원이라면 누구나 1주일 간의 여름 휴가를 즐길 수 있습니다. 오랜시간을 함께한 장기 근속자(3년/6년/10년)에게는 리프레쉬 휴가(유급)와 휴가비를 통해 고마운 마음을 전달합니다. 그 외에도 프로젝트를 성실히 이행하고 마무리한 경우 프로젝트 휴가, 프로젝트 수주 또는 성공적인 마무리 시에는 해외로 휴가를 떠날 수 있는 휴가비를 지급하고 있습니다.', },
         { title: 'Vacation', desc: '무더운 여름, 더피프티원 직원이라면 누구나 1주일 간의 여름 휴가를 즐길 수 있습니다. 오랜시간을 함께한 장기 근속자(3년/6년/10년)에게는 리프레쉬 휴가(유급)와 휴가비를 통해 고마운 마음을 전달합니다. 그 외에도 프로젝트를 성실히 이행하고 마무리한 경우 프로젝트 휴가, 프로젝트 수주 또는 성공적인 마무리 시에는 해외로 휴가를 떠날 수 있는 휴가비를 지급하고 있습니다.', },
       ]
     }
