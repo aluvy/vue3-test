cbceb02f4f70ae2d01fdf5ccabe95809c5535391
miok <aluvy_@naver.com>
2024-03-09 21:16:51 +0900

    수정

--- front/src/components/common/ScrollTop.vue
+++ front/src/components/common/ScrollTop.vue
@@ -24,20 +24,26 @@ export default {
   },
   methods: {
     scrollTop() {
-      if(this.isRun) return;
-      if(window.scrollY === 0) return;
+      window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+      })
+    },
+    // scrollTop() {
+    //   if(this.isRun) return;
+    //   if(window.scrollY === 0) return;
 
-      const speed = window.scrollY * 0.05;
+    //   const speed = window.scrollY * 0.05;
 
-      this.isRun = true;
-      this.intervalId = setInterval(() => {
-        if (window.scrollY === 0) {
-          this.isRun = false;
-          clearInterval(this.intervalId);
-        }
-        window.scroll(0, window.scrollY - speed);
-      }, 20);
-    }
+    //   this.isRun = true;
+    //   this.intervalId = setInterval(() => {
+    //     if (window.scrollY === 0) {
+    //       this.isRun = false;
+    //       clearInterval(this.intervalId);
+    //     }
+    //     window.scroll(0, window.scrollY - speed);
+    //   }, 20);
+    // }
   }
 }
 </script>
